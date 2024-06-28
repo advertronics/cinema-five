b3dc94d83b130b9e6efa55fcb3396af06a2984b7
advertronics <67376089+advertronics@users.noreply.github.com>
2024-06-28 22:39:08 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+'use client"
+
 import Image from "next/image"
 import styles from "./page.module.css"
 import ShowcaseSlider from "../../components/ShowcaseSlider"
@@ -5,7 +7,7 @@ import Search from "../../components/Search"
 import CategoriesSlider from "../../components/CategoriesSlider"
 import FAQ from "../../components/FAQ"
 
-import { getServerSession } from "next-auth"
+//import { getServerSession } from "next-auth"
 //import { authOptions } from "@/app/api/auth/[...nextauth]/route"
 import { generateSafcomAccessToken } from "@/safcomAccessToken"
 import { tryOutFetchTransaction } from "@/lib/callSafcomFetchFunction"
