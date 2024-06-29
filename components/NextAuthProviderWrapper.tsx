631661ef916e97a6224aead675b9a0085951623e
advertronics <67376089+advertronics@users.noreply.github.com>
2024-06-29 12:52:33 +0300

    Update NextAuthProviderWrapper.tsx

--- components/NextAuthProviderWrapper.tsx
+++ components/NextAuthProviderWrapper.tsx
@@ -3,5 +3,5 @@
 import { SessionProvider } from "next-auth/react"
 
 export const NextAuthProviderWrapper = ({ children }: { children: React.ReactNode }) => {
-  return (<SessionProvider>{children}</SessionProvider>)
+  return <SessionProvider>{children}</SessionProvider>
 }
