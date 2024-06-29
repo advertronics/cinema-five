b2e6485d0053be0a991d4eeec3ea825f381a61db
advertronics <67376089+advertronics@users.noreply.github.com>
2024-06-29 12:25:36 +0300

    Update route.ts

--- src/app/api/safPayments/route.ts
+++ src/app/api/safPayments/route.ts
@@ -17,7 +17,7 @@ export async function POST(req: Request) {
 
   const access_token = await generateSafcomAccessToken()
 
-  let { Amount, PhoneNumber, AccountReference } = requestData
+  let { AccountReference } = requestData
 
   let headers = new Headers()
   headers.append("Content-Type", "application/json")
